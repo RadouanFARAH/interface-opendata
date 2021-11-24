86f9a7ea200e6a7569eb8ebe407ad26df487894c
Radouan FARAH <redouane.farah.1995@gmail.com>
2021-11-24 23:56:52 +0100

    app de préqualification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-   url:"https://intranet.alamana.org.ma:3132/api",
+   url:"https://localhost:4200/api",
+  //  url:"https://intranet.alamana.org.ma:3132/api",
   //  url:"https://192.168.150.40:3132",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  url:"https://intranet.alamana.org.ma:3132/api",
+  url:"https://localhost:4200/api",
+  // url:"https://intranet.alamana.org.ma:3132/api",
   // url:"https://192.168.150.40:3132",
 };
 
